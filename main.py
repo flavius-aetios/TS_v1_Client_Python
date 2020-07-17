352edc48cae08930a91126983fc4c724664c694b
flavius-aetios <mihail25.98@gmail.com>
2020-07-17 13:11:10 +0700

    Add icon inline

--- main.py
+++ main.py
@@ -53,8 +53,10 @@ layout = [
     [sg.Submit('Подтвердить', pad=((405,0),1))]
 ]
 
-sg.set_global_icon(icon = r'Terminal.ico')
-window = sg.Window('TS_v1 COM Terminal', layout, icon="Terminal.ico")
+icon_base64 = b'iVBORw0KGgoAAAANSUhEUgAAAIAAAACACAMAAAD04JH5AAAAP1BMVEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABHcEyWpW0nAAAAFXRSTlP/+AyyWWb74i4Giqbrcw0R9SPHUAClWLX2AAACEklEQVR42u3b25KDIAwGYOyqnNTWuu//rLvWqdqpToH8kN2Z5Kp3+QYBEyrqmzmUAAQggL8CsJfCYfcAa3r/VTh8b+wTMDrdqvKh3bgAzF0xxd3MAL78D4EavGIMP6ip4wR0k2IdgN8hUJoXoNW6ANuisWbdnoZrCoZ/33i0KfkGmCpmQC0AAQhAAAIQgAD+F8CagRdQazdwAhqtbmhBBMDWc+GOFkQA6qVxAAvCAc2zccEKQgG23honqCAU8NK6IwXBI9B0eQTBcyCXIHwSZhLE7ANZBDE7YRZB1FacQxD5NsQLIusBvCC2IIELoisitCC+JAMLEmpCrCClKIUKkqpipCCtLAcKEvsCnCC1MYEJkjsjlCC9NQMJCL2hbW4vAssLqNyF9RG07so6CavU/KBlmJ4fsxER8kO2Ykp+xMuIlB/wOqblpxckxPzkkoyan1qUkvMTy3J6flpjAshPas0Q+SnNKSQ/oT3H5E8/oADlTz6iOchvQwJ1SHWQ/+r85+gt5pjuaPzHkE9AfDJgvH96/pkB+6Pa4/mXGbATnMz/3ID1uPxs/eUHLH8YnK7/AoB5DM73nxKA+U+r0/2vCMCa8/13aPrPMREBGUIAAhCAAAQgAAEI4ADQ9XXBcNun3euvqmTsPm5n/7yf/YID+xUP9ksu7Nd8+C86cV31aterXvyX3R4G1ut+nCEAAQiAHfAD1PNjIYVXmu0AAAAASUVORK5CYII='
+
+sg.set_global_icon(icon_base64)
+window = sg.Window('TS_v1 COM Terminal', layout)
 
 while True:
     event, values = window.read()
